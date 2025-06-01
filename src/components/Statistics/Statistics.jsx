3f5cecb4219de9f894a70381f14965ca3405784f
Evgeniy <evgnec@gmail.com>
2025-06-01 23:50:58 +0300

    upd

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React from 'react';
 import css from './Statistics.module.css';
 
+export default function Statistics({
+  good,
+  neutral,
+  bad,
+  countTotalFeedback,
+  countPositiveFeedbackPercentage,
+}) {
+  let percent = countPositiveFeedbackPercentage();
+  let className = '';
+  if (percent < 50) {
+    className = css.low;
+  } else if (percent < 75) {
+    className = css.medium;
+  } else {
+    className = css.high;
+  }
 
-export default function Statistics ({good, neutral, bad, countTotalFeedback, countPositiveFeedbackPercentage}){
-
-    let percent = countPositiveFeedbackPercentage();
-        let className = '';  
-        if (percent < 50) {          
-          className = css.low;
-        } else if (percent < 75) {
-          className = css.medium;
-        } else {
-          className = css.high;
-        }
-        
-        return(
-        <>
-        <ul className={css.feedbackList}>
+  return (
+    <>
+      <ul className={css.feedbackList}>
         <li className={css.listItem}>Good: {good}</li>
         <li className={css.listItem}>Neutral: {neutral}</li>
         <li className={css.listItem}>Bad: {bad}</li>
         <li className={css.listItem}>Total: {countTotalFeedback()}</li>
-        <li className={`${css.feedback} ${className}`}>Positive Feedback: {countPositiveFeedbackPercentage()} %</li>
-        </ul>
-        </>)
+        <li className={`${css.feedback} ${className}`}>
+          Positive Feedback: {countPositiveFeedbackPercentage()} %
+        </li>
+      </ul>
+    </>
+  );
 }
